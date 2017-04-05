0eb7d8537d585f675296daae090f79404166035d
Aviram (Duke) Moran <aviram.moran@gmail.com>
2017-04-05 09:28:20 -0500

    Simple GPIB C# Write / Read Code

---
+++ Connect_To_GPIB/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Connect_To_GPIB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.GPIB = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(214, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Send GPIB Command";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // GPIB
+            // 
+            this.GPIB.FormattingEnabled = true;
+            this.GPIB.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.GPIB.Items.AddRange(new object[] {
+            "5",
+            "22",
+            "23"});
+            this.GPIB.Location = new System.Drawing.Point(21, 14);
+            this.GPIB.Name = "GPIB";
+            this.GPIB.Size = new System.Drawing.Size(39, 21);
+            this.GPIB.TabIndex = 3;
+            this.GPIB.Text = "5";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(21, 117);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(202, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Read Back After Command Complete";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(21, 56);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(278, 48);
+            this.richTextBox2.TabIndex = 7;
+            this.richTextBox2.Text = "*IDN?";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(21, 140);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(278, 48);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 213);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.GPIB);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Simple GPIB Connection";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox GPIB;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Connect_To_GPIB/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Ivi.Visa.Interop;
+
+namespace Connect_To_GPIB
+{
+    public partial class Form1 : Form
+    {
+        private readonly ResourceManager _rm = new ResourceManager();
+        private readonly FormattedIO488 _io488Con = new FormattedIO488();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var resourceString = ("GPIB0::" + GPIB.Text + "::INSTR");
+            richTextBox1.Clear();
+            richTextBox1.BackColor = Color.White;
+            try
+            {
+                _io488Con.IO = (IMessage)_rm.Open(resourceString, AccessMode.NO_LOCK, 0);
+                _io488Con.IO.Clear();
+                _io488Con.WriteString(richTextBox2.Text);
+                if (checkBox1.CheckState == CheckState.Checked)
+                {
+                    richTextBox1.Text = _io488Con.ReadString();
+                }
+                else
+                {
+                    richTextBox1.BackColor = Color.LimeGreen;
+                    richTextBox1.Text = @"Command Execution Completed";
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                _io488Con.IO.Close();
+            }
+        }
+    }
+}
